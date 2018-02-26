f7d3f81526f33c93115ce30e533e902f55004be4
Trevor Sutcliffe <mrtsut@hotmail.com>
2018-02-26 14:51:22 -0600

    Created CheeseCategory in Models

--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -7,6 +7,11 @@ namespace CheeseMVC.Data
     {
         public DbSet<Cheese> Cheeses { get; set; }
 
+        //added -  creates a table named Categories in the database
+        public DbSet<CheeseCategory> Categories { get; set; }
+
+
+
         public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
             : base(options)
         { }
---
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class CheeseCategory
+    {
+        //Add ID and Name Property
+        public string name { get; set; }
+        public int ID { get; set; }   
+
+    }
+}
