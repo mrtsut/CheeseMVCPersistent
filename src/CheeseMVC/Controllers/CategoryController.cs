8d43e9a788e22bc33bd9790e9e712d5952ecff8b
Trevor Sutcliffe <mrtsut@hotmail.com>
2018-02-27 13:03:53 -0600

    added CategoryController

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly CheeseDbContext context;    //readonly allows for a one time write during init
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        //creates a private field context of type CheeseDbContext.
+        //mechanism to interact with objects stored in the database
+        //MVC framework creates an instance of CheeseDbContext and passes in into the constructor
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
