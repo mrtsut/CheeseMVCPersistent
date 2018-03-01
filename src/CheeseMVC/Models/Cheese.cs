63cfd03dce90027065cd0541fa9dd1efaf40178b
Trevor Sutcliffe <mrtsut@hotmail.com>
2018-03-01 16:12:33 -0600

    current error: I nvalid column name 'CategoryID'.'

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -7,8 +7,9 @@ namespace CheeseMVC.Models
         public string Name { get; set; }
         public string Description { get; set; }
         //public CheeseType Type { get; set; }  - replace with ChesseCategory object
-        public CheeseCategory Category { get; set; }
         public int CategoryID { get; set; }
+        public CheeseCategory Category { get; set; }
+        
         public int ID { get; set; }
     }
 }
