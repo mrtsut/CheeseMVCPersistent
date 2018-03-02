81b7e73f9fb1c4ed9935d52253498453c23dafe7
Trevor Sutcliffe <mrtsut@hotmail.com>
2018-03-02 10:55:30 -0600

    Updated CheeseDbContext

--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -10,11 +10,20 @@ namespace CheeseMVC.Data
         //added -  creates a table named Categories in the database
         public DbSet<CheeseCategory> Categories { get; set; }
 
+        // added in Part 3 Update CheeseDbContext
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<CheeseMenu> CheeseMenus { get; set; }
+
 
 
         public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
             : base(options)
         { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CheeseMenu>()
+                .HasKey(c => new { c.CheeseID, c.MenuID });
+        }
     }
 }
