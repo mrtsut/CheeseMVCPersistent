16118808c3d3cdc14a376a84dc790a45cf1b4383
Trevor Sutcliffe <mrtsut@hotmail.com>
2018-03-02 13:07:19 -0600

    Display AddMenuForm complete

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: Menu
+        public IActionResult Index()
+        {
+            List<Menu> menus = context.Menus.ToList();
+            return View(menus);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddMenuViewModel addMenuViewModel = new AddMenuViewModel();
+            return View(addMenuViewModel);
+        }
+
+    }
+}
+      
